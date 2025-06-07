a105e04e07f3bef2bbd1f3ef56011b26112409f2
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-06-07 00:57:18 +0000

    JWT completo

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
 import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
@@ -10,14 +13,23 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_bcrypt import Bcrypt  
+from flask_jwt_extended import JWTManager 
 
-# from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
+BCRYPT = Bcrypt(app) 
+app.BCRYPT = BCRYPT 
+
+jwt = JWTManager(app) 
+
+
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -40,7 +52,6 @@ setup_commands(app)
 # Add all endpoints form the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
 
-# Handle/serialize errors like a JSON object
 
 
 @app.errorhandler(APIException)
@@ -56,13 +67,13 @@ def sitemap():
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = 'index.html'
     response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # avoid cache memory
+    response.cache_control.max_age = 0  
     return response
 
 
